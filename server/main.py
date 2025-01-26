2d5822742bf45c8907ff4b4260993ea80f659ae6
Kuan Yi Wang <kuanyi.wang0807@gmail.com>
2025-01-26 02:11:55 -0500

    feat: sorta working minmax

--- server/main.py
+++ server/main.py
@@ -6,9 +6,10 @@ from collections import defaultdict
 FACE_WIDTH = 12
 TEST_WINDOW_TIME = 2
 try:
+    from screeninfo import get_monitors
     screen = get_monitors()[0]
     SCREEN_HEIGHT = screen.height
-except:
+except ImportError:
     SCREEN_HEIGHT = 1080
 
 class Chronometer:
@@ -23,20 +24,21 @@ class Chronometer:
         if not self.hc.extrema_tracking['active']:
             self.start_time = time.time()
             self.hc.extrema_tracking['active'] = True
-            print("Setting start time to", self.start_time)
+            print("Chronometer started at:", self.start_time)
 
     def stop(self):
         if self.hc.extrema_tracking['active']:
             self.hc.extrema_tracking['elapsed_time'] = time.time() - self.start_time
             self.total_time += self.hc.extrema_tracking['elapsed_time']
             self.hc.extrema_tracking['active'] = False
-            print("Setting elapsed_time ")
+            print("Chronometer stopped. Elapsed time:", self.hc.extrema_tracking['elapsed_time'])
 
     def reset(self):
         self.total_time = 0.0
         self.start_time = 0.0
         self.hc.extrema_tracking['elapsed_time'] = 0.0
         self.hc.extrema_tracking['active'] = False
+        print("Chronometer reset.")
 
     @property
     def time(self):
@@ -63,7 +65,7 @@ class HandChronometer:
         self.this_exercise_index = 0
         self.start_frame = 0
         self.stop_frame = 0
-        
+
         self.extrema_tracking = {
             'active': False,
             'elapsed_time': 0,
@@ -72,31 +74,33 @@ class HandChronometer:
         }
         self.chronometer = Chronometer(self)
 
-
     def fingers_up(self, hand_landmarks, handedness):
         tips = [4, 8, 12, 16, 20]
         count = 0
 
+        thumb_threshold = 0.02
+        finger_threshold = 0.02
+
         if handedness == "Right":
-            if hand_landmarks.landmark[tips[0]].x > hand_landmarks.landmark[tips[0] - 1].x:
+            if hand_landmarks.landmark[tips[0]].x > hand_landmarks.landmark[tips[0] - 1].x + thumb_threshold:
                 count += 1
         else:
-            if hand_landmarks.landmark[tips[0]].x < hand_landmarks.landmark[tips[0] - 1].x:
+            if hand_landmarks.landmark[tips[0]].x < hand_landmarks.landmark[tips[0] - 1].x - thumb_threshold:
                 count += 1
 
         for tip in tips[1:]:
-            if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip - 2].y:
+            if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip - 2].y - finger_threshold:
                 count += 1
 
         return count
 
     def process_hand_action(self, finger_count, current_time, current_frame):
-        if current_time - self.last_action_time >= 0.1 and current_time - self.last_action_time < self.action_cooldown:
+        if current_time - self.last_action_time < self.action_cooldown:
             return
-        
+
         FRAME_BUFFER = 100
 
-        if finger_count - 1 == 2:  # Two fingers up
+        if finger_count == 2:  # Two fingers up
             if not self.extrema_tracking['active'] and current_frame >= self.stop_frame + FRAME_BUFFER:
                 print("Two fingers detected. Activating.")
                 self.chronometer.start()
@@ -112,7 +116,7 @@ class HandChronometer:
                 self.extrema_tracking['max_y'] = float('-inf')
                 self.extrema_tracking['min_y'] = float('inf')
                 self.last_action_time = current_time
-        elif finger_count - 1 == 3:  # Three fingers up
+        elif finger_count == 3:  # Three fingers up
             self.chronometer.reset()
             self.this_exercise_index = (self.this_exercise_index + 1) % len(self.exercise_list)
             self.last_action_time = current_time
@@ -132,10 +136,12 @@ class HandChronometer:
             ret, frame = cap.read()
             if not ret:
                 break
+            
+            this_exercise = self.exercise_list[self.this_exercise_index]
 
             frame = cv2.flip(frame, 1)
             h, w, _ = frame.shape
-            current_time = time.time()  
+            current_time = time.time()
 
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             hand_result = self.hands.process(rgb_frame)
@@ -159,30 +165,56 @@ class HandChronometer:
                 right_ear_x, right_ear_y = right_ear.x * new_width, right_ear.y * new_height
                 left_ear_x, left_ear_y = left_ear.x * new_width, left_ear.y * new_height
                 propConstant = FACE_WIDTH / (((right_ear_y - left_ear_y) ** 2 + (right_ear_x - left_ear_x) ** 2) ** 0.5)
-                
-                right_shoulder = pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
-                left_shoulder = pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value]
-                
-                right_shoulder_x, right_shoulder_y = right_shoulder.x * new_width, right_shoulder.y * new_height
-                left_shoulder_x, left_shoulder_y = left_shoulder.x * new_width, left_shoulder.y * new_height
-                
-                avg_shoulder_y = ((right_shoulder_y + left_shoulder_y)/ 2) * propConstant
-                self.extrema_tracking['max_y'] = max(self.extrema_tracking['max_y'], avg_shoulder_y)
-                self.extrema_tracking['min_y'] = min(self.extrema_tracking['min_y'], avg_shoulder_y)
+
+                if this_exercise == "Squat" or this_exercise == "Pushup":
+                    right_shoulder = pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
+                    left_shoulder = pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value]
+
+                    right_y = right_shoulder.y * new_height * propConstant
+                    left_y = left_shoulder.y * new_height * propConstant
+                else:
+                    right_wrist = pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST.value]
+                    left_wrist= pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST.value]
+
+                    right_y = right_wrist.y * new_height * propConstant
+                    left_y = left_wrist.y * new_height * propConstant
+                    
+
+                avg_y = (right_y + left_y) / 2
+                self.extrema_tracking['max_y'] = max(self.extrema_tracking['max_y'], avg_y)
+                self.extrema_tracking['min_y'] = min(self.extrema_tracking['min_y'], avg_y)
 
             if hand_result.multi_hand_landmarks:
-                hand_landmarks, hand_handedness = hand_result.multi_hand_landmarks[0], hand_result.multi_handedness[0]
-                # for hand_landmarks, hand_handedness in zip(hand_result.multi_hand_landmarks, hand_result.multi_handedness):
-                handedness_label = hand_handedness.classification[0].label
-                finger_count = self.fingers_up(hand_landmarks, handedness_label)
+                right_ear = pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_EAR.value]
+                left_ear = pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_EAR.value]
 
-                self.process_hand_action(finger_count, current_time, current_frame)
+                right_ear_x, right_ear_y = right_ear.x * new_width, right_ear.y * new_height
+                left_ear_x, left_ear_y = left_ear.x * new_width, left_ear.y * new_height
+                propConstant = FACE_WIDTH / (((right_ear_y - left_ear_y) ** 2 + (right_ear_x - left_ear_x) ** 2) ** 0.5)
                 
-                self.mp_drawing.draw_landmarks(frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+                left_hip = pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_HIP.value]
+                right_hip = pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_HIP.value]
+                
+                left_shoulder = pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value]
+                right_shoulder = pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
+                
+                avg_threshold_y = ((left_hip.y + left_shoulder.y)/2 + (right_hip.y + right_shoulder.y)/2)/2 * new_height * propConstant 
+                for hand_landmarks, hand_handedness in zip(hand_result.multi_hand_landmarks, hand_result.multi_handedness):
+                    wrist_y = hand_landmarks.landmark[0].y * new_height * propConstant
+                    print(avg_threshold_y > wrist_y, avg_threshold_y, wrist_y)
+                    if avg_threshold_y < wrist_y:
+                        continue
+                    
+                    handedness_label = hand_handedness.classification[0].label
+                    finger_count = self.fingers_up(hand_landmarks, handedness_label)
 
-                x, y = int(hand_landmarks.landmark[0].x * w), int(hand_landmarks.landmark[0].y * h)
-                cv2.putText(frame, f"{handedness_label}: {finger_count - 1} fingers", (x, y - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    self.process_hand_action(finger_count, current_time, current_frame)
+
+                    self.mp_drawing.draw_landmarks(frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+
+                    x, y = int(hand_landmarks.landmark[0].x * w), int(hand_landmarks.landmark[0].y * h)
+                    cv2.putText(frame, f"{handedness_label}: {finger_count} fingers", (x, y - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
             cv2.imshow('Hand Chronometer', frame)
 
